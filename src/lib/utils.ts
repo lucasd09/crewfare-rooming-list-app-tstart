cc2ecf0e3cf1b797981625143fe348c892eb37fb
lucas dalan <lucasdalan09@gmail.com>
2025-08-25 17:47:13 -0300

    feat: added search input

---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const dateFormat = (dateString: string) => {
+  const [yyyy, mm, dd] = dateString.split("-").map(Number);
+  const date = new Date(yyyy, mm - 1, dd);
+
+  const day = date.toLocaleString("default", { day: "numeric" });
+  const month = date.toLocaleString("default", { month: "short" });
+
+  return { date, day, month, yyyy };
+};
+
+export const uppercaseFirstLetter = (str: string) =>
+  `${str.substring(0, 1).toUpperCase()}${str.substring(1)}`;
